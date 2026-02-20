f594320aa14ac9fea9db7baac5a92929e78edb18
Thyon3 <asnakemengesha80@gmail.com>
2026-02-20 13:39:02 -0500

    add create user dto

---
+++ src/adapters/repositories/user/dto/createUser.dto.ts
@@ -0,0 +1,14 @@
+// Module import
+import { Expose } from 'class-transformer';
+
+// Create a new user dto helper for db response
+class CreateUserDto {
+    @Expose()
+    id: number;
+    @Expose()
+    name: string;
+    @Expose()
+    email: string;
+}
+
+export { CreateUserDto };
