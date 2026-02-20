a2e28f6c8952291de414403930aaedba007047be
Thyon3 <asnakemengesha80@gmail.com>
2026-02-20 13:33:12 -0500

    add di binding for repositories

---
+++ src/infrastructure/di/repository.binding.ts
@@ -0,0 +1,29 @@
+// Module import
+import { ContainerModule } from 'inversify';
+import { Repository } from 'typeorm';
+// Repository import
+import { UserRepository } from '../../adapters/repositories/user/user.repository';
+import { UserEntity } from '../../adapters/repositories/user/user.repository.entity';
+import {
+    IUserRepository,
+    USER_REPOSITORY_TYPE,
+} from '../../adapters/repositories/user/user.repository.interface';
+// Database import
+import { AppDataSource } from '../database/db';
+
+// Bind Repository
+const repositoryBinding = new ContainerModule(bind => {
+    // Bind the TypeORM repository
+    bind<Repository<UserEntity>>(USER_REPOSITORY_TYPE.UserRepositoryEntity)
+        .toDynamicValue(() => {
+            return AppDataSource.getRepository(UserEntity);
+        })
+        .inRequestScope();
+
+    // Bind Repository
+    bind<IUserRepository>(USER_REPOSITORY_TYPE.UserRepository)
+        .to(UserRepository)
+        .inSingletonScope();
+});
+
+export { repositoryBinding };
