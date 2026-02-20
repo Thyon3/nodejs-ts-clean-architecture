30d4c9287ce6484b5090cc717273741fe05fb4a2
Thyon3 <asnakemengesha80@gmail.com>
2026-02-20 13:48:35 -0500

    define get user usecase interface

---
+++ src/usecases/user/getUser/getUser.interface.ts
@@ -0,0 +1,38 @@
+// domain import
+import {
+    IResponseErrorDomain,
+    ResponseErrorDomain,
+} from '../../../domains/error.domain';
+import {
+    IResponseDomain,
+    ResponseDomain,
+} from '../../../domains/response.domain';
+
+// GetUser DI identifiers
+const GET_USER_TYPE = {
+    GetUser: Symbol.for('GetUser'),
+    GetUserResponse: Symbol.for('GetUserResponse'),
+};
+
+// response type for the use case
+type ResponseGetUser = {
+    id: number;
+    name: string;
+    email: string;
+};
+
+// for GetUser response domain
+interface IGetUserResponse {
+    USER_NOT_FOUND: IResponseErrorDomain;
+    GET_USER_ERROR: IResponseErrorDomain;
+    OK: IResponseDomain<ResponseGetUser>;
+}
+
+// interface to implement the use case
+interface IGetUser {
+    execute(
+        email: string
+    ): Promise<ResponseDomain<ResponseGetUser> | ResponseErrorDomain>;
+}
+
+export { GET_USER_TYPE, IGetUser, IGetUserResponse, ResponseGetUser };
