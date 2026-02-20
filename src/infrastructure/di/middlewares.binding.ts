cb79729438f7dd398e5abd59ffc7af409ae6c569
Thyon3 <asnakemengesha80@gmail.com>
2026-02-20 13:32:55 -0500

    add di binding for middleware

---
+++ src/infrastructure/di/middlewares.binding.ts
@@ -0,0 +1,13 @@
+// Module import
+import { ContainerModule } from 'inversify';
+// Middleware import
+import { ErrorMiddleware } from '../middleware/error.middleware';
+import { LoggingMiddleware } from '../middleware/logging.middleware';
+
+// Bind Middlewares
+const middlewareBinding = new ContainerModule(bind => {
+    bind<LoggingMiddleware>(LoggingMiddleware).toSelf().inSingletonScope();
+    bind<ErrorMiddleware>(ErrorMiddleware).toSelf().inSingletonScope();
+});
+
+export { middlewareBinding };
