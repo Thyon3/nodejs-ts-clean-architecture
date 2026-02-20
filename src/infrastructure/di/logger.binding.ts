fa3fa9a516544501da5a323c98e00bd0b0b4931c
Thyon3 <asnakemengesha80@gmail.com>
2026-02-20 13:32:39 -0500

    add di binding for logger

---
+++ src/infrastructure/di/logger.binding.ts
@@ -0,0 +1,13 @@
+// Module import
+import { ContainerModule } from 'inversify';
+// Logger import
+import { Logger } from '../logging/logger';
+// Interface import
+import { ILogger, LOGGER_TYPE } from '../logging/logger.interface';
+
+// Bind Logger
+const loggerBinding = new ContainerModule(bind => {
+    bind<ILogger>(LOGGER_TYPE.Logger).to(Logger);
+});
+
+export { loggerBinding };
