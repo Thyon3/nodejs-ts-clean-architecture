fb33c5bde8dcfb9a9562003737a0d4299c44c687
Thyon3 <asnakemengesha80@gmail.com>
2026-02-20 13:38:23 -0500

    add user controller implementation

---
+++ src/adapters/controller/user/user.controller.ts
@@ -0,0 +1,47 @@
+// Module import
+import { Request, Response } from 'express';
+import { inject } from 'inversify';
+import {
+    controller,
+    httpPost,
+    request,
+    response,
+} from 'inversify-express-utils';
+// Domain import
+import { IResponseDomain } from '../../../domains/response.domain';
+// Middleware import
+import { validate } from '../../../infrastructure/middleware/validate.middleware';
+// Use case import
+import {
+    ADD_USER_TYPE,
+    IAddUser,
+    ResponseAddUser,
+} from '../../../usecases/user/addUser/addUser.interface';
+// Interface import
+import { IAddUserRequest } from './user.interface';
+// Schema import
+import { addUserSchema } from './user.schema';
+
+@controller('/user')
+class UserController {
+    public constructor(
+        @inject(ADD_USER_TYPE.AddUser) private readonly _addUser: IAddUser
+    ) { }
+
+    @httpPost('/', validate(addUserSchema))
+    async addUser(
+        @request() req: Request,
+        @response() res: Response
+    ): Promise<void> {
+        // Cast validated body into a user Domain instance
+        const user = req.body as unknown as IAddUserRequest; // Type assertion
+
+        // Call the use case
+        const { error, message, code, data }: IResponseDomain<ResponseAddUser> =
+            await this._addUser.execute(user);
+
+        res.status(code).json({ error, message, data });
+    }
+}
+
+export { UserController };
