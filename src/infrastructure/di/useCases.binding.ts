a9c19acd7c1fc8b659c6ab740c6cdb0b0241ef9a
Thyon3 <asnakemengesha80@gmail.com>
2026-02-20 13:33:28 -0500

    add di binding for usecases

---
+++ src/infrastructure/di/useCases.binding.ts
@@ -0,0 +1,15 @@
+// Module import
+import { ContainerModule } from 'inversify';
+// Use Case import
+import { AddUser } from '../../usecases/user/addUser/addUser';
+import {
+    ADD_USER_TYPE,
+    IAddUser,
+} from '../../usecases/user/addUser/addUser.interface';
+
+// Bind Use Cases
+const useCasesBinding = new ContainerModule(bind => {
+    bind<IAddUser>(ADD_USER_TYPE.AddUser).to(AddUser);
+});
+
+export { useCasesBinding };
