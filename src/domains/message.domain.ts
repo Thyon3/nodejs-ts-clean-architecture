317e721daecf3a0a13d8f1902bf82b834b468924
Thyon3 <asnakemengesha80@gmail.com>
2026-02-20 13:35:37 -0500

    define message domain models

---
+++ src/domains/message.domain.ts
@@ -0,0 +1,38 @@
+type TypeMessageDetail = {
+    token: string;
+    riskId: number;
+};
+
+// Message domain interface
+interface IMessageDomain {
+    text: string;
+    sender: string;
+    room: string;
+    topScore: TypeMessageDetail;
+    details: TypeMessageDetail[];
+}
+
+// Message domain class
+class MessageDomain implements IMessageDomain {
+    text: string;
+    sender: string;
+    room: string;
+    topScore: TypeMessageDetail;
+    details: TypeMessageDetail[];
+
+    constructor(
+        text: string,
+        sender: string,
+        room: string,
+        topScore: TypeMessageDetail,
+        details: TypeMessageDetail[]
+    ) {
+        this.text = text;
+        this.sender = sender;
+        this.room = room;
+        this.topScore = topScore;
+        this.details = details;
+    }
+}
+
+export { IMessageDomain, MessageDomain, TypeMessageDetail };
