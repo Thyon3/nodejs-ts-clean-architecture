88b6c06ca751c5f2eb3b9b61f1b713352af89a87
Thyon3 <asnakemengesha80@gmail.com>
2026-02-20 13:36:20 -0500

    define user domain models

---
+++ src/domains/user.domain.ts
@@ -0,0 +1,18 @@
+// User domain interface
+interface IUserDomain {
+    name: string;
+    email: string;
+}
+
+// User domain class
+class UserDomain implements IUserDomain {
+    name: string;
+    email: string;
+
+    constructor(name: string, email: string) {
+        this.name = name;
+        this.email = email;
+    }
+}
+
+export { IUserDomain, UserDomain };
