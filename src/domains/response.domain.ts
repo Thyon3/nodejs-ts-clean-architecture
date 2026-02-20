33d54278dc24fcb94d6cea2412d5fb9159a4f0f6
Thyon3 <asnakemengesha80@gmail.com>
2026-02-20 13:35:58 -0500

    define response domain models

---
+++ src/domains/response.domain.ts
@@ -0,0 +1,27 @@
+// Response domain interface
+interface IResponseDomain<T> {
+    error: boolean;
+    message: string;
+    code: number;
+    data?: T;
+}
+
+// Define a type alias for the constructor parameters, excluding 'data'
+type TypeResponseDomain<T> = Omit<IResponseDomain<T>, 'data'>;
+
+// Response domain class
+class ResponseDomain<T> implements IResponseDomain<T> {
+    error: boolean;
+    message: string;
+    code: number;
+    data: T;
+
+    constructor({ error, message, code }: TypeResponseDomain<T>, data: T) {
+        this.error = error;
+        this.message = message;
+        this.code = code;
+        this.data = data;
+    }
+}
+
+export { IResponseDomain, ResponseDomain };
