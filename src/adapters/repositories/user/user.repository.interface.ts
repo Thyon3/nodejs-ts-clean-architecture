be97929749acc18ea2ec0673b440037aed93256f
Thyon3 <asnakemengesha80@gmail.com>
2026-02-20 13:46:02 -0500

    define user repository interface

---
+++ src/adapters/repositories/user/user.repository.interface.ts
@@ -0,0 +1,19 @@
+// Domain import
+import { UserDomain } from '../../../domains/user.domain';
+// Dto import
+import { CreateUserDto } from './dto/createUser.dto';
+import { QueryUserDto } from './dto/queryUser.dto';
+
+// User Repository DI identifiers
+const USER_REPOSITORY_TYPE = {
+    UserRepository: Symbol.for('UserRepository'),
+    UserRepositoryEntity: Symbol.for('Repository<UserEntity>'),
+};
+
+// User Repository interface
+interface IUserRepository {
+    createUser(user: UserDomain): Promise<CreateUserDto>;
+    findUser(email: string): Promise<QueryUserDto | null>;
+}
+
+export { IUserRepository, USER_REPOSITORY_TYPE };
