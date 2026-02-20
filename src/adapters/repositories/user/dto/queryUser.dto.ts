70e2126e14cce98935e300decff0af41af3cfcfe
Thyon3 <asnakemengesha80@gmail.com>
2026-02-20 13:45:47 -0500

    add query user dto

---
+++ src/adapters/repositories/user/dto/queryUser.dto.ts
@@ -0,0 +1,18 @@
+// Module import
+import { Expose } from 'class-transformer';
+
+// Query user dto helper for db response
+class QueryUserDto {
+    @Expose()
+    id: number;
+    @Expose()
+    name: string;
+    @Expose()
+    email: string;
+    @Expose()
+    createdAt: Date;
+    @Expose()
+    updatedAt: Date;
+}
+
+export { QueryUserDto };
