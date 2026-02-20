55662953618ec436d28bed55b4bd3d9084f49504
Thyon3 <asnakemengesha80@gmail.com>
2026-02-20 13:37:27 -0500

    add user controller validation schema

---
+++ src/adapters/controller/user/user.schema.ts
@@ -0,0 +1,11 @@
+// Module import
+import Joi from 'joi';
+// Interface import
+import { IAddUserRequest } from './user.interface';
+
+const addUserSchema = Joi.object<IAddUserRequest>({
+    name: Joi.string().min(1).required(),
+    email: Joi.string().email({ minDomainSegments: 2 }).max(100).required(),
+}).required();
+
+export { addUserSchema };
