717c4a3c8ecfd433d2a9d019147ed114896411ff
Thyon3 <asnakemengesha80@gmail.com>
2026-02-20 13:38:08 -0500

    add request validation middleware

---
+++ src/infrastructure/middleware/validate.middleware.ts
@@ -0,0 +1,24 @@
+// Module import
+import { NextFunction, Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { ObjectSchema } from 'joi';
+// Domain import
+import { ErrorNames, ResponseErrorDomain } from '../../domains/error.domain';
+
+// This middleware function will validate the request body using a joi schema
+function validate(schema: ObjectSchema) {
+    return (req: Request, _res: Response, next: NextFunction) => {
+        const { error } = schema.validate(req.body);
+        if (error) {
+            throw new ResponseErrorDomain({
+                name: ErrorNames.BadRequestError,
+                error: true,
+                code: StatusCodes.BAD_REQUEST,
+                message: error.details[0].message,
+            });
+        }
+        return next();
+    };
+}
+
+export { validate };
