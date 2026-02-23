59bf754b8098f8f492287b54e4f9efd634d261e7
Thyon3 <asnakemengesha80@gmail.com>
2026-02-22 20:54:33 -0500

    add index to message room

--- src/infrastructure/database/migration/1714686029732-add-table-message.ts
+++ src/infrastructure/database/migration/1714686029732-add-table-message.ts
@@ -7,3 +7,8 @@ export class AddTableMessage1714686029732 implements MigrationInterface {
     await queryRunner.query(
       'CREATE TABLE "message_entity" ("id" SERIAL NOT NULL, "text" text NOT NULL, "sender" text NOT NULL, "room" text NOT NULL, "top_score" jsonb NOT NULL, "details" jsonb NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_45bb3707fbb99a73e831fee41e0" PRIMARY KEY ("id"))'
     );
+    await queryRunner.query(
+      'CREATE INDEX "idx_message_room" ON "message_entity" ("room") '
+    );
+  }
+
