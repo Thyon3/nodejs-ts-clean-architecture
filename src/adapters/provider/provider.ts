6413c826b6042d7f90dcfd75df9e056e533784b0
Thyon3 <asnakemengesha80@gmail.com>
2026-02-22 20:52:45 -0500

    add base provider imports

---
+++ src/adapters/provider/provider.ts
@@ -0,0 +1,5 @@
+// interface import
+import { type IEnvironmentConfig } from '../../infrastructure/config/environment.config';
+// infrastructure import
+import type { Http } from '../../infrastructure/http/http';
+
