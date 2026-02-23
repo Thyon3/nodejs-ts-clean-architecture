431564bfd1daf63840174caa464d9e95e1aadb62
Thyon3 <asnakemengesha80@gmail.com>
2026-02-22 20:54:20 -0500

    add message table to migration

--- src/infrastructure/database/migration/1714686029732-add-table-message.ts
+++ src/infrastructure/database/migration/1714686029732-add-table-message.ts
@@ -3,3 +3,7 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class AddTableMessage1714686029732 implements MigrationInterface {
   name = 'AddTableMessage1714686029732';
 
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      'CREATE TABLE "message_entity" ("id" SERIAL NOT NULL, "text" text NOT NULL, "sender" text NOT NULL, "room" text NOT NULL, "top_score" jsonb NOT NULL, "details" jsonb NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_45bb3707fbb99a73e831fee41e0" PRIMARY KEY ("id"))'
+    );
