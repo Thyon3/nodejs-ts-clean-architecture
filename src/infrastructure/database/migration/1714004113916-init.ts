31a05a14de2ad0c65b4ea805416b5251f293ef29
Thyon3 <asnakemengesha80@gmail.com>
2026-02-22 20:53:53 -0500

    add user table creation to init migration

--- src/infrastructure/database/migration/1714004113916-init.ts
+++ src/infrastructure/database/migration/1714004113916-init.ts
@@ -3,3 +3,9 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class Init1714004113916 implements MigrationInterface {
   name = 'Init1714004113916';
 
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      'CREATE TABLE "user_entity" ("id" SERIAL NOT NULL, "name" character varying(200) NOT NULL, "email" character varying(200) NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_b54f8ea623b17094db7667d8206" PRIMARY KEY ("id"))'
+    );
+  }
+
